f4cb3181f2ab2a1d0c859ee9d5f8ca517564a0e0
Grigor <grigorkalajdziev@gmail.com>
2024-12-08 13:00:17 +0100

    Adding localizations

--- src/components/Header/elements/MobileMenuSearch.js
+++ src/components/Header/elements/MobileMenuSearch.js
@@ -1,10 +1,12 @@
 import { IoIosSearch } from "react-icons/io";
+import { useLocalization } from "../../../context/LocalizationContext";
 
 const MobileMenuSearch = () => {
+  const { t } = useLocalization();  
   return (
     <div className="offcanvas-mobile-menu__search">
       <form>
-        <input type="search" placeholder="Search here" />
+        <input type="search" placeholder={t("search_here")} />
         <button type="submit">
           <IoIosSearch />
         </button>
