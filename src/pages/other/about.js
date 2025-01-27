61b04d19eb9e15035f961a2766326297f44bca5f
Grigor <grigorkalajdziev@gmail.com>
2025-01-27 22:44:33 +0100

    Changing pics

--- src/pages/other/about.js
+++ src/pages/other/about.js
@@ -14,7 +14,7 @@ import { useLocalization } from "../../context/LocalizationContext";
 const About = () => {
   const { t } = useLocalization();
   const [modalStatus, isOpen] = useState(false);
- 
+  const [additionalModalStatus, setAdditionalModalStatus] = useState(false);
 
   return (
     <LayoutTwo>
@@ -25,7 +25,10 @@ const About = () => {
       >
         <ul className="breadcrumb__list">
           <li>
-            <Link href="/home/trending" as={process.env.PUBLIC_URL + "/home/trending"}>
+            <Link
+              href="/home/trending"
+              as={process.env.PUBLIC_URL + "/home/trending"}
+            >
               <a>{t("home")}</a>
             </Link>
           </li>
@@ -41,13 +44,13 @@ const About = () => {
               <Col lg={8} className="ml-auto mr-auto">
                 {/* section title */}
                 <div className="about-title-container text-center">
-                  <p className="dark-title space-mb--35">{t("simply_or_white")}</p>
-                  <h2 className="title space-mb--15">
-                  {t("clever_unique_ideas")}
-                  </h2>
-                  <p className="title-text">
-                  {t("about_page_description")}
+                  <p className="dark-title space-mb--35">
+                    {t("simply_or_white")}
                   </p>
+                  <h2 className="title space-mb--15">
+                    {t("clever_unique_ideas")}
+                  </h2>
+                  <p className="title-text">{t("about_page_description")}</p>
                 </div>
               </Col>
             </Row>
@@ -66,7 +69,7 @@ const About = () => {
                     backgroundImage: `url(${
                       process.env.PUBLIC_URL +
                       "/assets/images/backgrounds/about-video-bg.png"
-                    })`
+                    })`,
                   }}
                 >
                   <p className="video-text video-text-left">
@@ -115,32 +118,62 @@ const About = () => {
                 <Row>
                   <Col md={6}>
                     <div className="about-widget space-mb--35">
-                      <h2 className="widget-title space-mb--25">{t("address")}</h2>
-                      <p className="widget-content">
-                      {t("address_value")}
-                      </p>
+                      <h2 className="widget-title space-mb--25">
+                        {t("address")}
+                      </h2>
+                      <p className="widget-content">{t("address_value")}</p>
                     </div>
                     <div className="about-widget space-mb--35">
-                      <h2 className="widget-title space-mb--25">{t("phone")}</h2>
-                      <p className="widget-content">{t("mobile")}: (+389) 78 / 343 – 377</p>
+                      <h2 className="widget-title space-mb--25">
+                        {t("phone")}
+                      </h2>
+                      <p className="widget-content">
+                        {t("mobile")}: (+389) 78 / 343 – 377
+                      </p>
                       <span>{t("phone")}: (+389) 46 / 207 – 770</span>
                     </div>
                     <div className="about-widget">
-                      <h2 className="widget-title space-mb--25">{t("email")}</h2>
+                      <h2 className="widget-title space-mb--25">
+                        {t("email")}
+                      </h2>
                       <p className="widget-content">{t("email_value")}</p>
                     </div>
+                    {/* <div className="additional-video-section space-mb--35">
+                      <h2 className="widget-title space-mb--25">
+                        {t("watch_our_video")}
+                      </h2>
+                      <ModalVideo
+                        channel="youtube"
+                        isOpen={additionalModalStatus}
+                        videoId="SD9ZmfM9exM" 
+                        onClose={() => setAdditionalModalStatus(false)}
+                      />
+                      <button
+                        onClick={() => setAdditionalModalStatus(true)}
+                        className="btn-play-video"
+                      >
+                        <img
+                          src={
+                            process.env.PUBLIC_URL +
+                            "/assets/images/icon/icon-play-100x100.png"
+                          }
+                          className="img-fluid"
+                          alt="Play Video"
+                        />
+                      </button>
+                    </div> */}
                   </Col>
                   <Col md={6}>
                     <div className="about-page-text">
                       <p className="space-mb--35">
-                      {t("about_page_extra_description")}
+                        {t("about_page_extra_description")}
                       </p>
                       <Link
                         href="/shop/left-sidebar"
                         as={process.env.PUBLIC_URL + "/shop/left-sidebar"}
                       >
                         <a className="lezada-button lezada-button--medium lezada-button--icon--left">
-                          <IoMdAdd />  {t("online_store")}
+                          <IoMdAdd /> {t("online_store")}
                         </a>
                       </Link>
                     </div>
