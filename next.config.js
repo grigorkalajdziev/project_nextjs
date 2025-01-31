2c19d20beac5075156ce4cb4d011cac288b3d5e5
Grigor <grigorkalajdziev@gmail.com>
2025-01-31 10:05:56 +0100

    Change next config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,5 @@
 module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; frame-src 'none'; connect-src 'self';"
-          },
-          {
-            key: "Referrer-Policy",
-            value: "no-referrer"
-          },
-          {
-            key: "Permissions-Policy",
-            value: "geolocation=(), microphone=(), camera=()"
-          }
-        ],
-      },
-    ];
-  },
+  env: {
+    PUBLIC_URL: ""
+  }
 };
