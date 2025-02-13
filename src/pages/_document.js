68d3303b561bbbcc6d5ead9fdfbaa15dc0f3b3e4
Grigor <grigorkalajdziev@gmail.com>
2025-02-13 16:19:42 +0100

    BreadcrumbList

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,9 +2,46 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
+    const breadcrumbJSONLD = {
+      "@context": "https://schema.org",
+      "@type": "BreadcrumbList",
+      "itemListElement": [
+        {
+          "@type": "ListItem",
+          "position": 1,
+          "name": "Kika Makeup and Beauty Academy",
+          "item": "https://www.kikamakeupandbeautyacademy.com"
+        },
+        {
+          "@type": "ListItem",
+          "position": 2,
+          "name": "About Us",
+          "item": "https://www.kikamakeupandbeautyacademy.com/about-us"
+        },
+        {
+          "@type": "ListItem",
+          "position": 3,
+          "name": "Contact",
+          "item": "https://www.kikamakeupandbeautyacademy.com/contact"
+        },
+        {
+          "@type": "ListItem",
+          "position": 4,
+          "name": "Shop",
+          "item": "https://www.kikamakeupandbeautyacademy.com/shop"
+        }
+      ]
+    };
+
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Inject breadcrumb structured data */}
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbJSONLD) }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
