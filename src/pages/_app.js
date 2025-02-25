aa78451f125ba927d9e6c7b113ac41ed596c3333
Grigor <grigorkalajdziev@gmail.com>
2025-02-25 12:34:20 +0100

    Rediretion remove

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import withReduxStore from "../lib/with-redux-store";
 import { Provider } from "react-redux";
-import { ToastProvider } from "react-toast-notifications";
+import { ToastProvider, useToasts } from "react-toast-notifications";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import { fetchProducts } from "../redux/actions/productActions";
@@ -13,9 +13,9 @@ import products from "../data/products.json";
 import "../assets/scss/styles.scss";
 import Preloader from "../components/Preloader";
 import { LocalizationProvider, useLocalization } from "../context/LocalizationContext";
-import HeaderTop from "../components/Header/HeaderTop"
+import HeaderTop from "../components/Header/HeaderTop";
 import CookieConsentToast from "../components/Cookies/CookieConsent";
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../pages/api/register";
 
 const libreBaskerville = Libre_Baskerville({
@@ -33,30 +33,29 @@ const ptSerif = PT_Serif({
 class MyApp extends App {
   constructor(props) {
     super(props);
-    this.persistor = persistStore(props.reduxStore);    
-    props.reduxStore.dispatch(fetchProducts(products)); 
+    this.persistor = persistStore(props.reduxStore);
+    props.reduxStore.dispatch(fetchProducts(products));
   }
 
   render() {
-    const { Component, pageProps, reduxStore } = this.props;    
+    const { Component, pageProps, reduxStore } = this.props;
     return (
       <Fragment>
-        <Head>         
-        </Head>
+        <Head />
         <div className={`${libreBaskerville.className} ${ptSerif.className}`}>
-        <ToastProvider placement="bottom-left">
-          <Provider store={reduxStore}>
-            <PersistGate loading={<Preloader />} persistor={this.persistor}>
-              <LocalizationProvider>             
-              <LocalizedHead />
-              <SessionHandler />
-               <HeaderTop />
-               <CookieConsentToast />                
-                  <Component {...pageProps} />                
-              </LocalizationProvider>
-            </PersistGate>
-          </Provider>
-        </ToastProvider>
+          <ToastProvider placement="bottom-left">
+            <Provider store={reduxStore}>
+              <PersistGate loading={<Preloader />} persistor={this.persistor}>
+                <LocalizationProvider>
+                  <LocalizedHead />
+                  <SessionHandler />
+                  <HeaderTop />
+                  <CookieConsentToast />
+                  <Component {...pageProps} />
+                </LocalizationProvider>
+              </PersistGate>
+            </Provider>
+          </ToastProvider>
         </div>
       </Fragment>
     );
@@ -68,24 +67,22 @@ const SessionHandler = () => {
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (!user) {        
-        router.push("/other/login-register"); // Redirect to login
+      if (!user) {      
       }
     });
-
-    return () => unsubscribe(); // Cleanup
-  }, []);
+    return () => unsubscribe();
+  }, [router]);
 
   return null;
-};
+}
 
 const LocalizedHead = () => {
   const { t } = useLocalization();
-  
+
   return (
     <Head>
       <title>{t("title")}</title>
-      <link rel="icon" href={process.env.PUBLIC_URL + "/favicon.ico"} />     
+      <link rel="icon" href={process.env.PUBLIC_URL + "/favicon.ico"} />
     </Head>
   );
 };
