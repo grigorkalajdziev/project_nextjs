beb664ec0d638334d55d1e45e110f9b877077693
Grigor <grigorkalajdziev@gmail.com>
2025-04-15 15:03:54 +0200

    BreadcrumbOne

--- src/components/Breadcrumb/BreadcrumbOne.js
+++ src/components/Breadcrumb/BreadcrumbOne.js
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+
 const BreadcrumbOne = ({ children, backgroundImage, pageTitle, className }) => {
+  const [scrollY, setScrollY] = useState(0);
+  useEffect(() => {
+    const handleScroll = () => setScrollY(window.scrollY);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div
       className={`breadcrumb-area space-pt--70 space-pb--70 ${
         className ? className : ""
       }`}
       style={{
-        backgroundImage: `url("${process.env.PUBLIC_URL + backgroundImage}")`
+        backgroundImage: `url("${process.env.PUBLIC_URL + backgroundImage}")`,
+        backgroundPositionY: `${scrollY * 0.5}px`
       }}
     >
       <Container>
