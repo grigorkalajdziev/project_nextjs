ac8ae82f38801d53234e135aa27e4b1b2ff31d3f
Grigor <grigorkalajdziev@gmail.com>
2025-10-17 17:56:29 +0200

    Fix typo in SCSS selector and update import paths for consistency
    - Corrected the typo in the `_product-description.scss` file from `&:last-chils` to `&:last-child`.
    - Updated import paths in `styles.scss` to remove the tilde (~) for consistency across third-party styles.
    - Removed commented-out language-specific styles for cleaner code.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  experimental: {    
-    forceSwcTransforms: true,
-  },
+  reactStrictMode: false,  
   env: {
     PUBLIC_URL: "", 
   },
