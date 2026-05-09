34ef8de80bbafe556a9171d50dd40fe77be14d5b
Grigor <grigorkalajdziev@gmail.com>
2026-05-09 14:13:02 +0200

    Remove unused layout components, product grid components, section titles, and related files to streamline the codebase.

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,11 +1,5 @@
-import BlogSidebar from "./BlogSidebar";
-import BlogPostGridWrapper from "./BlogPostGridWrapper";
-import BlogPostListWrapper from "./BlogPostListWrapper";
 import BlogPostSlider from "./BlogPostSlider";
 
 export {
-  BlogSidebar,
-  BlogPostGridWrapper,
-  BlogPostListWrapper,
   BlogPostSlider
 };
--- src/components/ProductThumb/index.js
+++ src/components/ProductThumb/index.js
@@ -1,15 +1,9 @@
 import ProductGridWrapper from "./ProductGridWrapper";
-import ProductGridTwoWrapper from "./ProductGridTwoWrapper";
-import ProductGridThreeWrapper from "./ProductGridThreeWrapper";
-import ProductGridFourWrapper from "./ProductGridFourWrapper";
 import ProductGridListWrapper from "./ProductGridListWrapper";
 import ProductWidgetWrapper from "./ProductWidgetWrapper";
 
 export {
-  ProductGridWrapper,
-  ProductGridTwoWrapper,
-  ProductGridThreeWrapper,
-  ProductGridFourWrapper,
+  ProductGridWrapper,  
   ProductGridListWrapper,
   ProductWidgetWrapper
 };
